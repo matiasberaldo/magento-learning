b7cbcef3efa89a19e9c6b6edfdfab98851a45e82
Matias Beraldo <matias.beraldo@gmail.com>
2019-03-25 21:29:49 +0100

    Add pencilInterface

--- app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
+++ app/code/SimplifiedMagento/FirstModule/Controller/Page/HelloWorld.php
@@ -2,15 +2,21 @@
 namespace SimplifiedMagento\FirstModule\Controller\Page;
 
 use Magento\Framework\App\ResponseInterface;
+use SimplifiedMagento\FirstModule\NotMagento\PencilInterface;
 
 /**
  *  HelloWorld class that extends Action\Action
  */
 class HelloWorld extends \Magento\Framework\App\Action\Action
 {
+  protected $pencilInterface;
+  public function __construct (Context $context, PencilInterface $pencilInterface) {
+    $this->pencilInterface = $pencilInterface;
+    parent::__construct($context);
+  }
 
   public function execute () {
-    echo "Hello World!";
+    echo $this->pencilInterface->getPencilType();
   }
 
 }
