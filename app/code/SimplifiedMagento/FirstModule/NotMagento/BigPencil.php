e1db24146db7897286e6af2c49e1c4a0f4d182e6
Matias Beraldo <matias.beraldo@gmail.com>
2019-03-25 22:54:04 +0100

    Testing big pencil

---
+++ app/code/SimplifiedMagento/FirstModule/NotMagento/BigPencil.php
@@ -0,0 +1,12 @@
+<?php
+namespace SimplifiedMagento\FirstModule\NotMagento;
+
+class BigPencil implements PencilInterface
+{
+  public function getPencilType () {
+    return "Big Pencil one";
+  }
+}
+
+
+ ?>
