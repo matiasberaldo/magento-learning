ef1c2cd9d5b6a14f47028aa9bfc0c161299974f4
Matias Beraldo <matias.beraldo@gmail.com>
2019-03-26 00:38:03 +0100

    Added second module base

--- app/code/SimplifiedMagento/SecondModule/registration.php
+++ app/code/SimplifiedMagento/SecondModule/registration.php
@@ -1,8 +1,8 @@
 <?php
 
  \Magento\Framework\Component\ComponentRegistrar::register(
-    "SecondModule", // The name of our preciated module
-    \Magento\Framework\Component\ComponentRegistrar::THEME, // We specify the theme that this module is for
+    \Magento\Framework\Component\ComponentRegistrar::MODULE, // The name of our preciated module
+    "SimplifiedMagento_SecondModule", // We specify the theme that this module is for
     __DIR__ // We specify the dir that this file is on
  );
 
