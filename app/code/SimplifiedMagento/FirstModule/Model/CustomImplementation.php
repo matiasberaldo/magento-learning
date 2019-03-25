f0a960abe13459771448e3870f848f71ff9ef563
Matias Beraldo <matias.beraldo@gmail.com>
2019-03-25 23:59:42 +0100

    Overriding Magento's PaymentTokenManagementInterface with our CustomImplementation

---
+++ app/code/SimplifiedMagento/FirstModule/Model/CustomImplementation.php
@@ -0,0 +1,16 @@
+<?php
+namespace SimplifiedMagento\FirstModule\Model;
+
+use Magento\Vault\Api\PaymentTokenManagementInterface;
+
+class CustomImplementation implements PaymentTokenManagementInterface
+{
+
+  function __construct(argument)
+  {
+    // code...
+  }
+}
+
+
+ ?>
